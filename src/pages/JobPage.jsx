a9a30fe196ae2735b26c71b1df59c912758b033f
Newsha Hosseiny Nik <newshahn@gmail.com>
2024-05-06 18:31:53 +0200

    feat: add addJob page, call post and delete api requests

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,19 @@
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this listing?'
+    );
+    if (!confirm) return;
+    deleteJob(jobId);
+    navigate('/jobs');
+  };
 
   return (
     <>
@@ -78,7 +87,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'>
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
+                >
                   Delete Job
                 </button>
               </div>
